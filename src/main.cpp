6b7023f0539bb2c389af8f0f157bd9ceba927d47
Alexandr <teletajp@yandex.ru>
2019-07-02 02:05:23 +0300

    modify1

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,41 @@
+#include <map>
+#include <iostream>
+#include "version.h"
 #include "spec_allocator.h"
+constexpr int factorial(unsigned int t)
+{
+       if(t)return t*factorial(t-1);
+       else return 1;
+}
 int main(int argc, char const *argv[])
 {
-    ma::spec_allocator<int, 32> al;
-     
+    std::map<int, int> num2fact_standart;
+    num2fact_standart.emplace(std::make_pair(0,factorial(0)));
+    num2fact_standart.emplace(std::make_pair(1,factorial(1)));
+    num2fact_standart.emplace(std::make_pair(2,factorial(2)));
+    num2fact_standart.emplace(std::make_pair(3,factorial(3)));
+    num2fact_standart.emplace(std::make_pair(4,factorial(4)));
+    num2fact_standart.emplace(std::make_pair(5,factorial(5)));
+    num2fact_standart.emplace(std::make_pair(6,factorial(6)));
+    num2fact_standart.emplace(std::make_pair(7,factorial(7)));
+    num2fact_standart.emplace(std::make_pair(8,factorial(8)));
+    num2fact_standart.emplace(std::make_pair(9,factorial(9)));
+
+    for(auto [k,v]:num2fact_standart)
+        std::cout << k << ' ' << v << std::endl;
+
+    std::map<int, int, std::less<int>, spec_allocator<std::pair<const int,int>, 10>> num2fact_nostandart;    
+    num2fact_nostandart.emplace(std::make_pair(0,factorial(0)));
+    num2fact_nostandart.emplace(std::make_pair(1,factorial(1)));
+    num2fact_nostandart.emplace(std::make_pair(2,factorial(2)));
+    num2fact_nostandart.emplace(std::make_pair(3,factorial(3)));
+    num2fact_nostandart.emplace(std::make_pair(4,factorial(4)));
+    num2fact_nostandart.emplace(std::make_pair(5,factorial(5)));
+    num2fact_nostandart.emplace(std::make_pair(6,factorial(6)));
+    num2fact_nostandart.emplace(std::make_pair(7,factorial(7)));
+    num2fact_nostandart.emplace(std::make_pair(8,factorial(8)));
+    num2fact_nostandart.emplace(std::make_pair(9,factorial(9)));
+    for(auto [k,v]:num2fact_nostandart)
+        std::cout << k << ' ' << v << std::endl; 
     return 0;
 }
--- src/spec_allocator.h
+++ src/spec_allocator.h
@@ -3,28 +3,31 @@
 #include <cstddef>
 #include <array>
 
-namespace ma
-{
 template<class T, std::size_t N>
 class spec_allocator
 {
     static_assert(N != 0, "zero allocating");
     using alloc_block_t = std::array<T, N>;
+    std::size_t free_size = N;
     std::size_t block_size = N;
+    alloc_block_t mem_pool;
     public:
         using value_type = T;
         spec_allocator() = default;
+        template <class U, std::size_t M> spec_allocator(const spec_allocator<U, M> &) noexcept {}
         T* allocate(std::size_t n)
         {
-
+            if(!free_size)  throw std::bad_alloc();
+            if(n != 1)      throw std::invalid_argument("can't allocate bigger then 1 element.");
+            return &mem_pool[--free_size];
         }
 
         void deallocate(T *p, size_t n){};
 
         template<class U> struct rebind {using other = spec_allocator<U,N>;};
 };
-}
+
 template <class T, class U, std::size_t N>
-bool operator==(const ma::spec_allocator<T,N>&, const ma::spec_allocator<U,N>&) { return true; }
+bool operator==(const spec_allocator<T,N>&, const spec_allocator<U,N>&) { return true; }
 template <class T, class U, std::size_t N>
-bool operator!=(const ma::spec_allocator<T,N>&, const ma::spec_allocator<U,N>&) { return false; }
+bool operator!=(const spec_allocator<T,N>&, const spec_allocator<U,N>&) { return false; }
